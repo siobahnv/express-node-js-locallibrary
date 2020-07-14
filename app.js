5ff5b525a33ab039109e02af70fb25075390485f
Heather Ransome <siobahnv@gmail.com>
2020-07-14 00:50:02 -0700

    Fixed for production.

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
